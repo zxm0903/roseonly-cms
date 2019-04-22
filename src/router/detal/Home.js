f30da69a6b641cf80e685bb03835f65653353ccf
17602845813 <1169534025@qq.com>
2019-04-22 09:01:42 +0800

    ceshi 3

--- src/router/detal/Home.js
+++ src/router/detal/Home.js
@@ -1,19 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 
-Vue.use(Router)
-
-const HomeRouter = new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-      {
-        path: '/',
-        name: 'home',
-        component: Home
-      }
+const HomeRouter =
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/Home.vue')
+    }
   
-    ]
-  })
+    
+
 
   export default HomeRouter
--- src/router/detal/Login.js
+++ src/router/detal/Login.js
@@ -1,18 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 
-Vue.use(Router)
 
-const LoginRouter = new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
+const LoginRouter =  
       {
         path: '/login',
         name: 'login',
-        component: Login
-      }  
-    ]
-  })
+        component: () => import('@/views/Login.vue')
+      }
+    
+
 
   export default LoginRouter
