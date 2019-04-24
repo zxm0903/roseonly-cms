2c0e181833143bce15439e92117a3903a49bde6d
zxm0903 <45614418+zxm0903@users.noreply.github.com>
2019-04-24 22:55:51 +0800

    zxm

---
+++ src/views/order/components/Consignment.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-dialog title="提示" :visible.sync="dialog" width="30%" :before-close="handleClose">
+    <el-form label-position="left" ref="form" :model="form" label-width="80px">
+      <el-form-item label="快递">
+        <el-select v-model="form.region" placeholder="请选择快递">
+          <el-option label="申通" value="shanghai"></el-option>
+          <el-option label="圆通" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="快递单号">
+        <el-input v-model="form.color"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible: false,
+       form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+    };
+  },
+  props: ["dialog"],
+  methods: {
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then(() => {
+          done();
+        })
+        .catch(() => {});
+    }
+  }
+};
+</script>
---
+++ src/views/order/components/GoodList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="goodlist">
+    <el-table :data="listdata" style="width: 100%">
+      <el-table-column >
+        <template slot-scope="props">
+          <img :src="props.row.url">
+          <!-- {{props.row}} -->
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="产品名称" width="180"></el-table-column>
+      <el-table-column prop="price" label="价格（元）" width="180"></el-table-column>
+      <el-table-column prop="num" label="数量"></el-table-column>
+      <el-table-column  label="小计（元）">
+          <template slot-scope="props">
+          
+          {{props.row.num * props.row.price}}
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+export default {
+  name: "goodlist",
+  data() {
+    return {
+      listdata: [
+        {
+          url: "http://pic.58pic.com/58pic/15/11/51/20E58PICNs4_1024.jpg",
+          name: "鲜花",
+          price: 185,
+          num: 4
+        },
+        {
+          url: "http://pic.58pic.com/58pic/15/11/51/20E58PICNs4_1024.jpg",
+          name: "鲜花",
+          price: "185.00",
+          num: '1'
+        }
+      ]
+    };
+  },
+  props: ["good"]
+};
+</script>
+<style scoped>
+img {
+  width: 80px;
+  height: 80px;
+}
+</style>
+
+
+
